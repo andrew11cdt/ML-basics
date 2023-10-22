9d80c1c9904b2c3818ec7816e8a8b761906c140d
Andrew Nguyen <80407443+andrew11cdt@users.noreply.github.com>
2023-10-22 15:43:05 -0700

    Update real_estate_price_size_predic.py

--- real_estate_price_size_predic.py
+++ real_estate_price_size_predic.py
@@ -79,25 +79,25 @@ def gradient_descent(x, y, w_in, b_in, alpha, num_iters, cost_function, gradient
     b = b_in
     w = w_in
     
-for i in range(num_iters):
-    # Calculate the gradient and update the parameters using gradient_function
-    dj_dw, dj_db = gradient_function(x, y, w , b)     
-
-    # Update Parameters using equation (3) above
-    b = b - alpha * dj_db                            
-    w = w - alpha * dj_dw                            
-
-    # Save cost J at each iteration
-    if i<100000:      # prevent resource exhaustion 
-        J_history.append( cost_function(x, y, w , b))
-        p_history.append([w,b])
-    # Print cost every at intervals 10 times or as many iterations if < 10
-    if i% math.ceil(num_iters/10) == 0:
-        print(f"Iteration {i:4}: Cost {J_history[-1]:0.2e} ",
-              f"dj_dw: {dj_dw: 0.3e}, dj_db: {dj_db: 0.3e}  ",
-              f"w: {w: 0.3e}, b:{b: 0.5e}")
-
-return w, b, J_history, p_history #return w and J,w history for graphing
+   for i in range(num_iters):
+       # Calculate the gradient and update the parameters using gradient_function
+       dj_dw, dj_db = gradient_function(x, y, w , b)     
+   
+       # Update Parameters using equation (3) above
+       b = b - alpha * dj_db                            
+       w = w - alpha * dj_dw                            
+   
+       # Save cost J at each iteration
+       if i<100000:      # prevent resource exhaustion 
+           J_history.append( cost_function(x, y, w , b))
+           p_history.append([w,b])
+       # Print cost every at intervals 10 times or as many iterations if < 10
+       if i% math.ceil(num_iters/10) == 0:
+           print(f"Iteration {i:4}: Cost {J_history[-1]:0.2e} ",
+                 f"dj_dw: {dj_dw: 0.3e}, dj_db: {dj_db: 0.3e}  ",
+                 f"w: {w: 0.3e}, b:{b: 0.5e}")
+   
+   return w, b, J_history, p_history #return w and J,w history for graphing
 
 # initialize parameters
 w_init = 0
